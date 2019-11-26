ebda55461efb37ddd064804bac799fff6a1f0210
ubunturifa <jstrfaheem065@gmail.com>
2019-11-26 14:24:16 +0530

    Home response,

--- server/Handler.js
+++ server/Handler.js
@@ -1,9 +1,31 @@
-function home(){
+function home(response){
     console.log("Excecuting home handler!\n");
+
+    var htmlFile = 
+    '<html>' +
+    '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; charset-UTF-8"/>'+
+    '</head>'+
+    '<body>' +
+        '<form action="/review" method="post">' +
+            '<textarea name="text" rows="20" cols="60"></textarea>' +
+            '<br>' +
+            '<input type="submit" value="SUBMIT TEXT"/>' +
+        '</form>' +
+    '</body>'+
+    '</body>' +
+    '</html>'
+    ;
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write(htmlFile);
+    response.end();
 }
 
-function review(){
+function review(response){
     console.log("Executing review handler!\n");
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("HELLO FROM REVIEW!");
+    response.end();
 }
 
 exports.home = home;
--- server/Server.js
+++ server/Server.js
@@ -6,10 +6,10 @@ function startServer(route, handle){
         var pathname = url.parse(request.url).pathname;
         console.log("New Requiest!");
         console.log("Requested for : " + pathname);
-        route(handle, pathname);
-        response.writeHead(200, {"Content-Type" : "text/plain"});
-        response.write("HELLO FROM OUR APPLICATION!");
-        response.end();
+        route(handle, pathname, response);
+        // response.writeHead(200, {"Content-Type" : "text/plain"});
+        // response.write("HELLO FROM OUR APPLICATION!");
+        // response.end();
     }).listen(8888);
 }
 
