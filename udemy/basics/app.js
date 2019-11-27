d447e509dd68e1a9565f473b89638208b6e1648b
ubunturifa <jstrfaheem065@gmail.com>
2019-11-27 22:44:37 +0530

    Handling the post request,

--- udemy/basics/app.js
+++ udemy/basics/app.js
@@ -1,5 +1,6 @@
 // Included the http core module
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
 
 // http is a core module to create a server
 // createServer is a method of http core module which will
@@ -12,7 +13,10 @@ http.createServer((request, response)=>{
     // console.log(request.url, request.method, request.headers);
     // response.end("Page is not available now!");
     // process.exit();
-    if(request.url=='/'){
+
+    const method = request.method;
+    const url = request.url;
+    if(url=='/'){
         response.setHeader('Content-Type', 'text/html');
         response.write(
             `<html>
@@ -28,6 +32,25 @@ http.createServer((request, response)=>{
             </html>`);
         return response.end();
     }
+
+    if(url==='/message' && method==='POST'){
+        const body = [];
+
+        request.on('data', (chunck)=>{
+            body.push(chunck);
+        });
+
+        request.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('postrequest.txt', message);
+        });
+        response.statusCode = 302;
+        response.setHeader('Location' , '/');
+        // response.writeHead(302, {'Location' : '/'});
+        return response.end();
+    }
+
     response.setHeader('Content-Type', 'text/html');
     response.write('<p>404 PAGE NOT FOUND!</p><br><p>Next</p>');
     response.end();
