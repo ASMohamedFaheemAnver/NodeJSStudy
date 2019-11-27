9a52b0ade4b342010c67a5da07e50130f2b91f49
ubunturifa <jstrfaheem065@gmail.com>
2019-11-27 08:07:37 +0530

    Arrays with map,

---
+++ udemy/7array_and_method/arrays.js
@@ -0,0 +1,11 @@
+const hobbies = ['Sports', 'Cooking'];
+
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+console.log(hobbies.map(hobby => {
+    return "Hobby : " + hobby;
+}));
+
+console.log(hobbies);
