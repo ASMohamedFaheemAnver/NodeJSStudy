7f88fd90c9e7fa41dd9fdefe4d5a8d71cea3eb1e
ubunturifa <jstrfaheem065@gmail.com>
2019-11-27 13:09:25 +0530

    Routing requests,

--- udemy/basics/app.js
+++ udemy/basics/app.js
@@ -9,6 +9,27 @@ var http = require('http');
 // to get and send packets/data!
 
 http.createServer((request, response)=>{
-    console.log(request);
-    response.end("Page is not available now!");
+    // console.log(request.url, request.method, request.headers);
+    // response.end("Page is not available now!");
+    // process.exit();
+    if(request.url=='/'){
+        response.setHeader('Content-Type', 'text/html');
+        response.write(
+            `<html>
+                <head>
+                    <title>My first page!</title>
+                </head>
+                <body>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message"/>
+                        <button type="submit">SUBMIT</button>
+                    </form>
+                </body>
+            </html>`);
+        return response.end();
+    }
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<p>404 PAGE NOT FOUND!</p><br><p>Next</p>');
+    response.end();
+
 }).listen(3000);
