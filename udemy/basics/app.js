1cd90b7ad4c04c9049bc3a14a5d639301870d905
ubunturifa <jstrfaheem065@gmail.com>
2019-11-28 10:29:49 +0530

    Asynchronous writing,

--- udemy/basics/app.js
+++ udemy/basics/app.js
@@ -40,15 +40,28 @@ http.createServer((request, response)=>{
             body.push(chunck);
         });
 
-        request.on('end', ()=>{
+        // We returning this code because we don't want to execute 
+        // 404 page and setHeader after the below code listening to
+        // 'end' emiter
+        return request.on('end', ()=>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('postrequest.txt', message);
+            // We are bloking the code by execute writeFileSync
+            // fs.writeFileSync('postrequest.txt', message);
+
+            // We use writeFile to wirte the file asynchronously
+            // and we write call back function handle if there is any 
+            // error in our code!
+            fs.writeFile('postrequest.txt', message ,(error)=>{
+                if(error){
+                    console.log(error);
+                }
+                response.statusCode = 302;
+                response.setHeader('Location' , '/');
+                // response.writeHead(302, {'Location' : '/'});
+                return response.end();
+            });
         });
-        response.statusCode = 302;
-        response.setHeader('Location' , '/');
-        // response.writeHead(302, {'Location' : '/'});
-        return response.end();
     }
 
     response.setHeader('Content-Type', 'text/html');
