e7e18fb4b628e4000b3a4e4221e273f3b2ab7541
ubunturifa <jstrfaheem065@gmail.com>
2019-12-01 18:10:04 +0530

    Middleware,

--- udemy/expressstudy/app.js
+++ udemy/expressstudy/app.js
@@ -1,14 +1,19 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-app.use((request, response, next)=>{
+app.use('/add-product', (request, response, next)=>{
+    response.send('<h1>You can add your product!</h1>');
+});
+
+
+app.use('/', (request, response, next)=>{
     response.send('<h1>Hello from express!</h1>');
 });
 
 const PORT = 3000;
-
-const server = http.createServer(app).listen(PORT);
+app.listen(PORT);
+// const server = http.createServer(app).listen(PORT);
 console.log('Server is running on : localhost:' + PORT);
