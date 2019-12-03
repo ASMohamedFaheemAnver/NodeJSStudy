d6edf425e24f79e8a9c3de4afae0ddce1e0ec682
ubunturifa <jstrfaheem065@gmail.com>
2019-12-03 22:06:05 +0530

    Page not found route,

--- udemy/expressstudy/app.js
+++ udemy/expressstudy/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const path = require('path');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,7 +17,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>404 PAGE NOT FOUND!</h1>');
+    res.sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
 });
 
 const PORT = 3000;
