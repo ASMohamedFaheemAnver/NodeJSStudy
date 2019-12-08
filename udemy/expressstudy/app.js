2cb526f777d24f7947fadbdb2a6d8ca9565724cd
ubunturifa <jstrfaheem065@gmail.com>
2019-12-08 17:20:57 +0530

    Replaced all html to pug,

--- udemy/expressstudy/app.js
+++ udemy/expressstudy/app.js
@@ -24,7 +24,7 @@ app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).render(path.join(__dirname, 'views', 'page-not-found.pug'));
 });
 
 const PORT = 3000;
--- udemy/expressstudy/routes/admin.js
+++ udemy/expressstudy/routes/admin.js
@@ -8,7 +8,8 @@ const rootDir = require('../util/path');
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.pug'));
+    res.render(path.join(rootDir, 'views', 'add-product.pug'));
 });
 
 const products = [];
@@ -16,9 +17,11 @@ const products = [];
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next)=>{
     // console.log(JSON.stringify(req.body));
-    products.push({title: req.body.title});
-    res.redirect('/');
-
+    if(req.body.title){
+        products.push({title: req.body.title});
+        return res.redirect('/');
+    }
+    res.send('<p>PLEASE ENTER DATA TO SUBMIT!</p>');
 });
 
 exports.routes = router;
