bc4482a415589686c3b43de646296bf27fb1bf23
rifacoder <jstrfaheem065@gmail.com>
2020-02-23 23:27:07 +0530

    Downloading invoices with stream data(data chunks)!,

--- udemy/expressstudy/controllers/shop.js
+++ udemy/expressstudy/controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
+const fs = require("fs");
+const path = require("path");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -163,5 +165,44 @@ exports.getOrders = (req, res, next) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
+    });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) {
+        return next(new Error("No order found!"));
+      }
+      if (order.user._id.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized!"));
+      }
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   // inline, attachment
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     "inline; filename='" + invoiceName + "'"
+      //   );
+      //   res.send(data);
+      // });
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Type", "application/pdf");
+      // inline, attachment
+      res.setHeader(
+        "Content-Disposition",
+        "inline; filename='" + invoiceName + "'"
+      );
+      file.pipe(res);
+    })
+    .catch(err => {
+      next(err);
     });
 };
