7edad6fabb4fca31dcebc8dd5ec87e83d6a4d7a2
rifacoder <jstrfaheem065@gmail.com>
2020-02-24 01:51:35 +0530

    Client side async request and dom manipulation,

--- udemy/expressstudy/controllers/admin.js
+++ udemy/expressstudy/controllers/admin.js
@@ -202,21 +202,23 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findOne({ _id: prodId, userId: req.user._id })
     .then(prod => {
       fileHelper.deleteFile(prod.imageUrl);
       return prod.delete();
     })
     .then(_ => {
-      res.redirect("/admin/products");
+      // res.redirect("/admin/products");
+      res.status(200).json({ message: "SUCCESS!" });
     })
     .catch(err => {
-      console.log(err);
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "FAIL TO DELETE!" });
+      // console.log(err);
+      // const error = new Error(err);
+      // error.httpStatusCode = 500;
+      // return next(error);
     });
   // Product.deleteOne({ _id: prodId, userId: req.user._id })
   //   .then(_ => {
