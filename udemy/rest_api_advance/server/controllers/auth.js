d011c9fcec178abd9362323f2df151d2ab202f6d
rifacoder <jstrfaheem065@gmail.com>
2020-02-25 14:43:04 +0530

    JWT token,

--- udemy/rest_api_advance/server/controllers/auth.js
+++ udemy/rest_api_advance/server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../model/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   console.log(req.body);
@@ -27,6 +28,44 @@ exports.signup = (req, res, next) => {
     })
     .then(result => {
       res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error("No use exist!");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        "i_use_rifa_to_secure",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
