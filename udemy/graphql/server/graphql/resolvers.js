20c1b5a59447289388a19f0de0bda55f22d141bf
rifacoder <jstrfaheem065@gmail.com>
2020-03-01 00:42:27 +0530

    Create user and client,

--- udemy/graphql/server/graphql/resolvers.js
+++ udemy/graphql/server/graphql/resolvers.js
@@ -1,8 +1,24 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../model/user");
+
 module.exports = {
-  hello() {
-    return {
-      text: "Hello world!",
-      views: 1
-    };
+  createUser: async function(/*args*/ { userInput }, req) {
+    // const email = args.userInput.email;
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User exists!");
+      throw error;
+    }
+
+    const hash = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hash
+    });
+
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   }
 };
