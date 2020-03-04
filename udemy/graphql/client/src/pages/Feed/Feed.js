71a9a4117e2e5a596602da50fc5b79643fbf9e5c
rifacoder <jstrfaheem065@gmail.com>
2020-03-05 00:06:31 +0530

    Nodejs testing with macha and chai!,

--- udemy/graphql/client/src/pages/Feed/Feed.js
+++ udemy/graphql/client/src/pages/Feed/Feed.js
@@ -270,15 +270,21 @@ class Feed extends Component {
 
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts++;
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
             updatedPosts.unshift(post);
           }
           return {
+            totalPosts: updatedPosts,
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
