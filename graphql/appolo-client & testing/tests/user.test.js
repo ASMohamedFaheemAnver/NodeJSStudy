20e9a1c2127914c88261c7e908f4ee067f5f4f4d
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-10-24 18:25:30 +0530

    Testing createUser mutation,

--- graphql/appolo-client & testing/tests/user.test.js
+++ graphql/appolo-client & testing/tests/user.test.js
@@ -1,7 +1,32 @@
-import { getFirstName } from "./example/util.js";
+import "cross-fetch/polyfill";
+import ApolloBoost, { gql } from "apollo-boost";
+import prisma from "../src/prisma";
 
-test("Should return first name when given full name", () => {
-  const firstName = getFirstName("Andrew Mead");
+const client = new ApolloBoost({
+  uri: "http://localhost:4000",
+});
 
-  expect(firstName).toBe("Andrew");
+test("Should create a new user.", async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: {
+          name: "Freedom"
+          email: "jstrfaheem065@gmail.com"
+          password: "password"
+        }
+      ) {
+        token
+        user {
+          id
+        }
+      }
+    }
+  `;
+
+  const response = await client.mutate({ mutation: createUser });
+  const exists = await prisma.exists.User({
+    id: response.data.createUser.user.id,
+  });
+  expect(exists).toBe(true);
 });
