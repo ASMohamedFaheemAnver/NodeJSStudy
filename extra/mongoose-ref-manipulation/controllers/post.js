70ef1240bbad6796262cd41616b3e1005aee246f
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-11-02 19:38:53 +0530

    Last tweak plz,

--- extra/mongoose-ref-manipulation/controllers/post.js
+++ extra/mongoose-ref-manipulation/controllers/post.js
@@ -2,23 +2,39 @@ const Post = require("../model/post");
 const User = require("../model/user");
 
 exports.createPost = async (req, res, next) => {
-  const user = await User.findById(req.params.userId);
-  const post = new Post({ ...req.body, user: user._id });
-  await post.save();
-  user.posts.push(post);
-  await user.save();
-
+  // const user = await User.findById(req.params.userId);
+  const post = new Post({ ...req.body, user: req.params.userId });
+  post.save().then((pPost) => {
+    return User.findById(pPost.user, (err, user) => {
+      if (user) {
+        user.posts.push(pPost);
+        user.save();
+      }
+    });
+  });
+  // user.posts.push(post);
+  // await user.save();
   return res.status(200).json(post);
 };
 
 exports.deletePost = async (req, res, next) => {
-  //console.log(req.params.postId);
-  const deletedPost = await Post.findByIdAndDelete(req.params.postId);
-  //console.log(deletedPost);
-  const user = await User.findById(deletedPost.user);
-  //console.log(deletedPost._id);
-  user.posts.pull(req.params.postId);
-  //console.log(user.posts);
-  user.save();
-  return res.status(200).json(deletedPost);
+  let deletedPost;
+
+  return Post.findById(req.params.postId, (err, post) => {
+    deletedPost = post;
+    return post.remove((err) => {
+      if (!err) {
+        User.update(
+          { _id: post.user },
+          { $pull: { posts: post._id } },
+          (err, numberAffected) => {
+            console.log(numberAffected);
+            return res.status(200).json(numberAffected);
+          }
+        );
+      } else {
+        console.log(err);
+      }
+    });
+  });
 };
--- extra/mongoose-ref-manipulation/controllers/ugly/post.js
+++ extra/mongoose-ref-manipulation/controllers/ugly/post.js
@@ -1,5 +1,5 @@
-const Post = require("../../model/post");
-const User = require("../../model/user");
+const Post = require("../model/post");
+const User = require("../model/user");
 
 exports.createPost = async (req, res, next) => {
   const user = await User.findById(req.params.userId);
@@ -12,24 +12,13 @@ exports.createPost = async (req, res, next) => {
 };
 
 exports.deletePost = async (req, res, next) => {
-  let deletedPost;
-
-  Post.findById(req.params.postId, (err, post) => {
-    deletedPost = post;
-    return post.remove((err) => {
-      if (!err) {
-        User.update(
-          { _id: post.user },
-          { $pull: { posts: post._id } },
-          (err, numberAffected) => {
-            console.log(numberAffected);
-          }
-        );
-      } else {
-        console.log(err);
-      }
-    });
-  });
-
+  //console.log(req.params.postId);
+  const deletedPost = await Post.findByIdAndDelete(req.params.postId);
+  //console.log(deletedPost);
+  const user = await User.findById(deletedPost.user);
+  //console.log(deletedPost._id);
+  user.posts.pull(req.params.postId);
+  //console.log(user.posts);
+  user.save();
   return res.status(200).json(deletedPost);
 };
