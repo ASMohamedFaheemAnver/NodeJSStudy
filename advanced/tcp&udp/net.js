27acf59dd02cdc57241166ca0c595ed2c3b1317f
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-11-24 10:27:37 +0530

    More awsome chat logics,

--- advanced/tcp&udp/net.js
+++ advanced/tcp&udp/net.js
@@ -2,16 +2,31 @@ const server = require("net").createServer();
 let counter = 0;
 let sockets = {};
 
+function timestamp() {
+  const now = new Date();
+  return `${now.getHours()}:${now.getMinutes()}`;
+}
+
 server.on("connection", socket => {
   console.log("Client connected.");
   socket.id = counter++;
-  sockets[socket.id] = socket;
-  socket.write("Welcome new client!\n");
+  // socket.write("Welcome new client!\n");
+  socket.write("Please type your name : ");
 
   socket.on("data", data => {
+    if (!sockets[socket.id]) {
+      socket.name = data.toString().trim();
+      sockets[socket.id] = socket;
+      socket.write(`Welcome ${socket.name}!\n`);
+      return;
+    }
+
     // console.log("data is : ", data);
     Object.entries(sockets).forEach(([key, cs]) => {
-      cs.write(`${socket.id}, data is : ${data}`);
+      if (socket.id == key) {
+        return;
+      }
+      cs.write(`${socket.name}@${timestamp()} : ${data}`);
     });
   });
 
