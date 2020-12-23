631ed70e771e93936a9d58ec46db44fde0b8478f
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-23 21:57:53 +0530

    Compare operators,

--- advanced/mongodb/db/db.js
+++ advanced/mongodb/db/db.js
@@ -4,10 +4,10 @@ const uri = "mongodb://127.0.0.1:27017/";
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-async function getDB() {
+async function getDB(collection = "playground") {
   try {
     await client.connect();
-    const db = client.db("playground");
+    const db = client.db(collection);
     return db;
   } catch (err) {
     throw err;
--- advanced/mongodb/operator/lt.js
+++ advanced/mongodb/operator/lt.js
@@ -2,6 +2,6 @@ const getDB = require("../db/db");
 
 (async () => {
   const db = await getDB("movie-datas");
-  const movies = await db.collection("movies").find({ runtime: { $ne: 60 } }).toArray();
+  const movies = await db.collection("movies").find({ runtime: { $lt: 40 } }).toArray();
   console.log(movies);
 })();
