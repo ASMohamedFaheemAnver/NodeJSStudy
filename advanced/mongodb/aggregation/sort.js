cdc729fc1a5912a205929b3b3c3672f9aa7e04ab
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-29 10:39:22 +0530

    Sort,

---
+++ advanced/mongodb/aggregation/sort.js
@@ -0,0 +1,14 @@
+const getDB = require("../db/db");
+
+(async () => {
+  const db = await getDB("analytics");
+  const persons = await db
+    .collection("persons")
+    .aggregate([
+      { $match: { gender: "female" } },
+      { $group: { _id: { state: "$location.state" }, totalPersons: { $sum: 1 } } },
+      { $sort: { totalPersons: -1 } },
+    ])
+    .toArray();
+  console.log(persons);
+})();
