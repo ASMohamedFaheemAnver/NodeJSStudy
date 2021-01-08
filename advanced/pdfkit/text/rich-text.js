08a4b138372b938c3758af0633c3575d3feeca8b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-08 10:34:56 +0530

    Rich text,

---
+++ advanced/pdfkit/text/rich-text.js
@@ -0,0 +1,20 @@
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+
+const doc = new PDFDocument();
+
+doc.pipe(fs.createWriteStream("rich.pdf"));
+
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;";
+
+doc
+  .fillColor("green")
+  .text(lorem.slice(0, 500), {
+    width: 465,
+    continued: true,
+  })
+  .fillColor("red")
+  .text(lorem.slice(500))
+  .text("hell yah!");
+doc.end();
