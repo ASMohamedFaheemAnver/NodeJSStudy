136b69864f52dfacb3867e2d1db3d5285c56faf7
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-23 11:29:30 +0530

    Broadcasting messages to all workers,

--- advanced/cluster-and-child-process/cluster/cluster.js
+++ advanced/cluster-and-child-process/cluster/cluster.js
@@ -1,12 +1,27 @@
 const cluster = require("cluster");
 const os = require("os");
 
+const numberOfUsersInDB = function () {
+  this.count = this.count || 5;
+  this.count = this.count * this.count;
+  return this.count;
+};
+
 if (cluster.isMaster) {
   const cpus = os.cpus().length;
   console.log(`forking for ${cpus} cups`);
   for (let i = 0; i < cpus; i++) {
     cluster.fork();
   }
+
+  const updateWorkers = () => {
+    const usersCount = numberOfUsersInDB();
+    Object.values(cluster.workers).forEach((worker) => {
+      worker.send({ usersCount });
+    });
+  };
+
+  setInterval(updateWorkers, 10000);
 } else {
   require("./server");
 }
--- advanced/cluster-and-child-process/cluster/server.js
+++ advanced/cluster-and-child-process/cluster/server.js
@@ -1,11 +1,18 @@
 const http = require("http");
 const pid = process.pid;
 
+let usersCount = 0;
+
 http
   .createServer((req, res) => {
     for (let i = 0; i < 1e7; i++);
-    res.end(`handling by processor : ${pid}`);
+    res.write(`handling by processor : ${pid}\n`);
+    res.end(`users : ${usersCount}`);
   })
   .listen(8000, () => {
     console.log(`started process : ${pid}`);
   });
+
+process.on("message", (msg) => {
+  usersCount = msg.usersCount;
+});
