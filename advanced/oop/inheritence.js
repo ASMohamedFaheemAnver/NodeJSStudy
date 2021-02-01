aab41a3a688bc522831ef263fe8968fb445ecaee
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-01 13:19:20 +0530

    Inheritence,

---
+++ advanced/oop/inheritence.js
@@ -0,0 +1,19 @@
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}.`;
+};
+
+function Magazine(title, author, year, month) {
+  Book.call(this, title, author, year);
+  this.month = month;
+}
+Magazine.prototype = Object.create(Book.prototype);
+
+const magazine = new Magazine("Hacker boooooooy!", "FreeDoM", "2018", "Jan");
+
+console.log(magazine.getSummary());
