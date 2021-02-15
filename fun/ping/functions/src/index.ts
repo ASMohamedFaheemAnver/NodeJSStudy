4b0026994c28d73968fef45b24a2e9f6edf2a2ae
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-15 14:48:06 +0530

    Error handling,

--- fun/ping/functions/src/index.ts
+++ fun/ping/functions/src/index.ts
@@ -4,10 +4,14 @@ import axios from "axios";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const doneSleepHeroku = functions.pubsub
+export const dontSleepHeroku = functions.pubsub
   .schedule("every 20 minutes")
   .onRun(async (context) => {
-    const res = await axios.get("https://hit-point.herokuapp.com/ping");
-    console.log(res);
-    return res;
+    try {
+      await axios.get("https://hit-point.herokuapp.com/ping");
+      // console.log(res.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+    return 0;
   });
