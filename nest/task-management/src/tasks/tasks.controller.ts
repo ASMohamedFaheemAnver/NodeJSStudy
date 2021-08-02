8619273669bddbfd01b194ca87a5ddf6fae10f2c
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-08-02 17:12:11 +0530

    Get and post request,

--- nest/task-management/src/tasks/tasks.controller.ts
+++ nest/task-management/src/tasks/tasks.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Response } from 'express';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(/*@Res() res: Response*/): Task[] {
+    // return res.status(HttpStatus.OK).send(this.tasksService.getAllTasks());
+    return this.tasksService.getAllTasks();
+  }
+
+  /*@Post()
+  createTask(@Body() body): Task {
+    return this.tasksService.createTask({
+      title: body.title,
+      description: body.description,
+    });
+  }*/
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask({
+      title: title,
+      description: description,
+    });
+  }
 }
--- nest/task-management/src/tasks/tasks.service.ts
+++ nest/task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(taskData: { title: string; description: string }): Task {
+    const task: Task = {
+      id: new Date().getTime().toString(),
+      ...taskData,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
