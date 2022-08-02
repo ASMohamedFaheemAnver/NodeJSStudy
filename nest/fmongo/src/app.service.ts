332d5df0465d02581fd0fbd6f4fa08948d53e5d9
FreeDoM <jstrfaheem065@gmail.com>
2022-08-02 16:13:15 +0530

    Perfect logging,

--- nest/fmongo/src/app.service.ts
+++ nest/fmongo/src/app.service.ts
@@ -5,20 +5,21 @@ import { User, UserDocument, UserSchema } from './schemas/user.schema';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async play() {
     const newUser = new this.userModel({ name: 'udev', age: 25 });
     try {
-      // await newUser.save();
+      await newUser.save();
     } catch (e) {
-      Logger.log(e);
+      this.logger.error(e);
     }
     // const users = await this.userModel.findOne().exec();
     // console.log({ users });
     const aggregatedUsers = await this.userModel.aggregate([
       { $match: { age: { $gte: 25 } } },
     ]);
-    console.log({ aggregatedUsers });
+    this.logger.log({ aggregatedUsers });
   }
 }
