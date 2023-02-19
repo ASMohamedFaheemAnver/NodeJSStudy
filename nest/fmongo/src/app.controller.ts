bb87bca8e26dbc72130e0933ff1aa069416925e0
FreeDoM <jstrfaheem065@gmail.com>
2023-02-19 22:10:09 +0530

    Find collection in a transaction explained,

--- nest/fmongo/src/app.controller.ts
+++ nest/fmongo/src/app.controller.ts
@@ -4,11 +4,23 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    appService.play();
+    appService.session();
   }
 
   @Get()
-  getHello(): { message: string } {
+  root(): { message: string } {
     return { message: 'server is up and running' };
+  }
+
+  @Get('/create')
+  create(): { message: string } {
+    this.appService.create();
+    return { message: 'done!' };
+  }
+
+  @Get('/session')
+  session(): { message: string } {
+    this.appService.session();
+    return { message: 'done!' };
   }
 }
--- nest/fmongo/src/app.service.ts
+++ nest/fmongo/src/app.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectConnection() private connection: mongoose.Connection,
+  ) {}
 
-  async play() {
+  async create() {
     const newUser = new this.userModel({ name: 'udev', age: 25 });
     try {
       await newUser.save();
@@ -21,5 +24,47 @@ export class AppService {
       { $match: { age: { $gte: 25 } } },
     ]);
     this.logger.log({ aggregatedUsers });
+  }
+
+  async session() {
+    await this.userModel.deleteMany();
+    const session = await this.connection.startSession();
+    session.startTransaction();
+    try {
+      // Create user with rollback session
+      const newUser = new this.userModel({ name: 'udev', age: 25 });
+      await newUser.save({ session });
+
+      // Returning null since we are committing session after find
+      const userWithoutSession = await this.userModel.findOne();
+      console.log({ userWithoutSession });
+
+      // Will return created one since we are in same session
+      const userWitSession = await this.userModel.findOne().session(session);
+      console.log({ userWitSession });
+
+      //  Updating user
+      userWitSession.age += 1;
+      await userWitSession.save({ session });
+
+      // Find update user
+      // Even if we got element it won't be updated one, since this query can be happen in multiple replicas
+      const findUpdatedUserWithoutSession = await this.userModel.findById(
+        userWitSession.id,
+      );
+      console.log({ findUpdatedUserWithoutSession });
+
+      // Find update user with session
+      const findUpdatedUserWithSession = await this.userModel
+        .findById(userWitSession.id)
+        .session(session);
+      console.log({ findUpdatedUserWithSession });
+      await session.commitTransaction();
+      session.endSession();
+    } catch (e) {
+      await session.abortTransaction();
+      session.endSession();
+      throw e;
+    }
   }
 }
